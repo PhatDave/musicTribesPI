a06f7e7d1bd86e2a7a352380fda5a0274bf20bf1
PhatPhuckDave <kosmodiskclassic0@gmail.com>
2022-03-09 16:15:08 +0100

    Add more models

--- customAuth/models.py
+++ customAuth/models.py
@@ -1,6 +1,13 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db.models import *
 
+from tribes.models import *
+
 
 class User(AbstractUser):
-    isMemberOf = ForeignKey("tribes.Tribe", on_delete=CASCADE, null=True)
+    pass
+
+class UserTribeMember(Model):
+    user = ForeignKey(settings.AUTH_USER_MODEL, on_delete=CASCADE)
+    tribe = ForeignKey(Tribe, on_delete=CASCADE)
--- tribes/models.py
+++ tribes/models.py
@@ -9,9 +9,32 @@ class Tribe(Model):
     # logo = URLField
     genre = CharField(max_length=32, null=True, default=None)
 
+class Message(Model):
+    user = ForeignKey(settings.AUTH_USER_MODEL, on_delete=CASCADE)
+    tribe = ForeignKey(Tribe, on_delete=CASCADE)
+    content = CharField(max_length=128)
+    date = DateTimeField(auto_now_add=True)
+
 
 class Playlist(Model):
     tribe = ForeignKey(Tribe, on_delete=CASCADE)
     owner = ForeignKey(settings.AUTH_USER_MODEL, on_delete=SET_DEFAULT, default="None", null=True)
     name = CharField(max_length=128, default="")
     description = CharField(max_length=256, null=True, default=None)
+
+class Song(Model):
+    playlist = ForeignKey(Playlist, on_delete=CASCADE)
+    link = URLField(max_length=256)
+    title = CharField(max_length=128)
+    artist = CharField(max_length=32)
+    duration = IntegerField()
+
+class UserLike(Model):
+    user = ForeignKey(settings.AUTH_USER_MODEL, on_delete=CASCADE)
+    song = ForeignKey(Song, on_delete=CASCADE)
+
+class UserComment(Model):
+    user = ForeignKey(settings.AUTH_USER_MODEL, on_delete=CASCADE)
+    song = ForeignKey(Song, on_delete=CASCADE)
+    content = CharField(max_length=512)
+    date = DateTimeField(auto_now_add=True)
