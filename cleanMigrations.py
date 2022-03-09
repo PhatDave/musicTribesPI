64ea8698df12637aa8c96fe78a8ccf4a328735bf
PhatPhuckDave <kosmodiskclassic0@gmail.com>
2022-03-09 16:57:05 +0100

    Add migration cleaner

---
+++ cleanMigrations.py
@@ -0,0 +1,9 @@
+import os
+
+for folder in os.listdir("."):
+    if os.path.isdir(folder):
+        for innerFolder in os.listdir(folder):
+            if innerFolder == "migrations":
+                for file in os.listdir(f'{folder}\\{innerFolder}'):
+                    if '__' not in file:
+                        os.remove(f'{folder}\\{innerFolder}\\{file}')
