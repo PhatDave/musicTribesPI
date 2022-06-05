45f218eb89ca0de5250df624a3d886a6aff2f5c4
aleksic-ivan <ivan.aleksic@student.ferit.hr>
2022-06-05 15:19:33 +0200

    First version of navbar

--- tribes/views.py
+++ tribes/views.py
@@ -5,10 +5,10 @@ from tribes.models import *
 
 
 def index(request):
-    context = {}
     html = 'tribes/index.html'
-
     tribe = Tribe.objects.all()[0]
     print(tribe.getMembers())
-
+    context = {
+        'nav': 'index',
+    }
     return render(request, html, context)
